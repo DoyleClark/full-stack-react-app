4c8f6302dac4847a666b5dffe14c51d813f91732
jac85295@uga.edu <jac85295@uga.edu>
2020-11-18 15:51:43 -0800

    Frontend code added

--- server/api/cities.js
+++ server/api/cities.js
@@ -14,7 +14,7 @@ router.get('/', function(req, res){
 router.post('/', function(req, res){
     var city = req.body.city;
 
-    cities.insert(city, function(err, result){
+    Cities.insert(city, function(err, result){
         if(err)
             return res.json(err);
         return res.json(result);
